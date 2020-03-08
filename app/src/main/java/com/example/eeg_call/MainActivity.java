dbdd384bec20bfed4eddb6fca48e53f9a08b5d5e
HAYOUNG LEE <l4538@naver.com>
2020-03-08 20:54:32 +0900

    fix

--- app/src/main/java/com/example/eeg_call/MainActivity.java
+++ app/src/main/java/com/example/eeg_call/MainActivity.java
@@ -8,8 +8,13 @@ import android.Manifest;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,16 +22,30 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     public static SharedPreferences sf;
     private boolean isRegistered;
-
+    private LinearLayout frame1;
+    private ImageView btn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sf = getSharedPreferences("config",MODE_PRIVATE);
         initFCM();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frame1 = (LinearLayout)findViewById(R.id.layout1);
+        btn = (ImageView) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"클릭",Toast.LENGTH_SHORT).show();
+            }
+        });
+        textView = (TextView)findViewById(R.id.token);
     }
 
     public void initFCM(){
@@ -39,20 +58,24 @@ public class MainActivity extends AppCompatActivity {
                             return;
                         }
                         String token = task.getResult().getToken();
+                        textView.setText(token);
                         Log.d("FCM", "Token : " + token);
-                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT);
                         checkRegister();
                     }
                 });
     }
 
     public void checkRegister(){
-        sf = getSharedPreferences("config",MODE_PRIVATE);
+
         isRegistered = sf.getBoolean("check",false);
         if(isRegistered == false){
+            frame1.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.INVISIBLE);
             Log.d("debug","register false");
         }
         else{
+            frame1.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.VISIBLE);
             Log.d("debug","register true");
         }
     }
